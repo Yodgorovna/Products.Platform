74152aafd82ddc4ad0385ec64e402dbb4b0bc0bb
Yodgorovna <lobargoziyeva9@gmail.com>
2023-11-10 00:35:07 +0500

    databse succses

---
+++ src/Products.WebApi/Configurations/Extensions/DataExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Products.DataAccess.DbContexts;
+
+namespace Products.WebApi.Configurations.Extensions
+{
+    public static class DataExtensions
+    {
+        public static void ApplyMigrations(this WebApplication app)
+        {
+            using (var scope = app.Services.CreateScope())
+            {
+                var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+                db.Database.Migrate();
+            }
+        }
+    }
+}
